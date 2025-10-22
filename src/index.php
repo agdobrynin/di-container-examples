ad5f1da74132035a2a634acdfcc92b19a7643a7d
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-10-22 21:28:48 +0400

    dev: Update design result tests.

--- src/index.php
+++ src/index.php
@@ -6,8 +6,14 @@ $start = microtime(true);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\test_group('DiContainer config by definitions', static fn() => include 'di/index.php');
-\test_group('DiContainer config by PHP attributes', static fn() => include 'attributes/index.php');
-
-print PHP_EOL . 'time: ' . round(microtime(true) - $start, 4) . ' sec.';
-print PHP_EOL . 'memory: ' . \mem() . PHP_EOL;
+try {
+    \test_group('DiContainer config by definitions', static fn() => include 'di/index.php');
+    \test_group('DiContainer config by PHP attributes', static fn() => include 'attributes/index.php');
+} catch (\AssertionError $exception) {
+    print test_title('Assert failed','💥');
+    print $exception;
+} finally {
+    print test_title('', '⏱');
+    print PHP_EOL . 'time: ' . round(microtime(true) - $start, 4) . ' sec.';
+    print PHP_EOL . 'memory: ' . \mem() . PHP_EOL;
+}
